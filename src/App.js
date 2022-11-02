409186979a47f373736ab716b6baf9bc8f8f2b71
Alex Lux <alexlux@live.com>
2022-11-02 04:14:29 -0700

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      {/* The sidebar  */}
+      <div class="sidebar">
+        <a class="active" href="#home">
+          Home
         </a>
-      </header>
-    </div>
+        <a href="#news">News</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+      </div>
+
+      {/* Page content  */}
+      <div class="content">..</div>
+    </>
   );
-}
+};
 
 export default App;
